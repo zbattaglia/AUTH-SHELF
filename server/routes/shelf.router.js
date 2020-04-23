49ba7dde39f731a3490d79fa129e3fe1960dcd26
Mark Spannbauer <msmarkieer@gmail.com>
2020-04-23 13:38:48 -0500

    Items now display on the page

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
 /**
  * Get all of the items on the shelf
  */
-router.get('/', (req, res) => {
-    res.sendStatus(200); // For testing only, can be removed
+router.get('/', rejectUnauthenticated, (req, res) => {
+    let queryText = 'SELECT * FROM "item" WHERE user_id = $1 ORDER BY id;';
+    pool.query(queryText, [req.user.id]).then(result => {
+        res.send(result.rows);
+    })
+    .catch(error => {
+        console.log('error selectin * from item', error);
+        res.sendStatus(500);
+    });
 });
 
 
--- src/components/InfoPage/InfoPage.js
+++ src/components/InfoPage/InfoPage.js
@@ -1,16 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios'
 
 // This is one of our simplest components
 // It doesn't have local state, so it can be a function component.
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is, so it doesn't need 'connect()'
 
-const InfoPage = () => (
-  <div>
-    <p>
-      Shelf Page
-    </p>
-  </div>
-);
+class InfoPage extends Component {
+  state = { 
+    shelfitems: []
+  }
 
+  componentDidMount() {
+    const config = {
+      headers: { 'Content-Type': 'application/json' },
+      withCredentials: true,
+    };
+
+    axios.get(`/api/shelf`, config)
+     .then( (response) => {
+       console.log(response)
+       this.setState({
+         shelfitems: response.data
+       })
+       
+     }).catch( (error) => {
+       alert('Bad things happened...')
+       console.log('Error in get /api/shelf', error)
+     })
+  }
+
+  render() { 
+    return ( 
+      <div>
+        <p>Shelf Items:</p>
+        {this.state.shelfitems.map( item => (
+          <div width="500px" height="600px" margin="auto">
+            <img src={item.image_url} width="500px" height="500px"></img>
+            <h3>{item.description} --- <button>DELETE</button></h3>
+            
+          </div>
+        ))
+      }
+      </div>
+     );
+  }
+}
+ 
 export default InfoPage;
